65bc048ac9443d6c5b4e9f8a386cb1c384cf8054
Thomas Krijnen <t.krijnen@gmail.com>
2023-07-02 21:44:49 +0200

    Add .mjs file

---
+++ application/puppeteer-pdf.mjs
@@ -0,0 +1,19 @@
+import puppeteer from 'puppeteer';
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  await page.setViewport({width: 1920, height: 1080});
+  await page.goto(process.argv[2], {waitUntil: 'networkidle0'});
+  await page.addStyleTag({ content: `.MuiPaper-root { 
+      border-radius: 0 !important; 
+      box-shadow: none !important;
+    }
+    .MuiTreeItem-iconContainer {
+      display: none !important;
+    }`
+  });
+  await page.pdf({ path: process.argv[3], format: 'A4' });
+  await browser.close();
+})();
